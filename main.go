b99f0815c36fca843a3601872781eec1e8b85ef3
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-03-25 22:26:25 -0300

    Templates de crear

--- main.go
+++ main.go
@@ -9,16 +9,25 @@ import (
 // Llamado a nuestro template
 var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 func main() {
-	//Solicitud para acceder a la funcion inicio
+	// Solicitud para acceder a la funcion inicio
 	http.HandleFunc("/", Inicio)
+	// Solicitud para mostrar la plantilla crear
+	http.HandleFunc("/crear", Crear)
 	// Log que indica por consola que el servidor esta corriendo
 	log.Println("Servidor corriendo...")
 	// Indicamos el servidor en el que estara corriendo la aplicacion
 	http.ListenAndServe(":8080", nil)
 }
 
+// Funcion para mostrar la plantilla de Inicio
 func Inicio(w http.ResponseWriter, r *http.Request){
 	//fmt.Fprintf(w,"Hola Go")
 	// Accedemos al contenido de la plantilla inicio
 	plantillas.ExecuteTemplate(w, "inicio", nil)
+}
+
+// Funcion para mostrar la plantilla de crear
+func Crear(w http.ResponseWriter, r *http.Request){
+	// Accedemos al contenido de la plantilla crear
+	plantillas.ExecuteTemplate(w, "crear", nil)
 }
